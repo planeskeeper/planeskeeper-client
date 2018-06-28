bbd34b38d00c69f69b90e55f3f67677e6245e24c
Chris L Chapman <38057494+ChrisSeattle@users.noreply.github.com>
2018-06-27 23:11:11 -0700

    Update home-view.js

--- scripts/views/home-view.js
+++ scripts/views/home-view.js
@@ -2,14 +2,14 @@
 
 var app = app || {};
 
-(function (module) { // start IIFE
-  let cardView = {};
+(function(module){ // start IIFE
+  let cardView = {}; 
 
   cardView.initIndexPage = () => {
-    console.log('init Index Page');
-    app.showOnly('home');
+    console.log('init Index Page'); 
+    app.showOnly('home'); 
   }; // end cardView.initIndexPage 
-
+  
   cardView.searchTemplate = cardObj => {
     let template = Handlebars.compile($('#card-template').text());
     return template(cardObj);
@@ -19,22 +19,25 @@ var app = app || {};
     event.preventDefault();
     var cardName = $('#card-name-search').val();
     $.getJSON(`https://api.magicthegathering.io/v1/cards?name=${cardName}`, function (json) {
-      app.Card.loadAll('search', json.cards);
-      $('#main-search').html('');
-      app.Card.search.map(a => a.toHtml('search'));
+      app.Card.loadAll('search', json.cards); 
+      $('#main-search').html(''); 
+      app.Card.search.map(a => a.toHtml('search')); 
       // listner for action on each card in search result list  
       $('.add-card').on('click', e => {
-        e.preventDefault();
+        e.preventDefault(); 
         // note - e.target.id is the same as $(this).attr('id')
         // but only if we are inside function(e) and not arrow func
-        let idChoice = e.target.id;
-        let action = 'add-card';
+        let idChoice = e.target.id; 
+        let user = 1; 
+        let action = 'add-card'; 
         console.log(`On Card id: ${idChoice}`);
-        console.log(`Your action: ${action}`);
+        console.log(`For user: ${user}`); 
+        console.log(`Your action: ${action}`); 
+        app.Card.edit(idChoice, user, action); 
       }); // end listner for each card in search result list
     });
   }; // end getCArdsApi, which is our search-button handler
-
+  
 
   // listner for main page search
   $('#search-button').click(getCardsApi);
